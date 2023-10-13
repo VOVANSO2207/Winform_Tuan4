165564c4d28369fafef4e672ca34b5829a36a062
vanso <126582742+VOVANSO2207@users.noreply.github.com>
2023-10-13 10:58:35 +0700

    Add files via upload

---
+++ Winform_Tuan4/Form_Bai5_C2.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Winform_Tuan4
+{
+    partial class frm_Bai5_C2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Bai5_C2));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblDefail = new System.Windows.Forms.Label();
+            this.lvThongTin = new System.Windows.Forms.ListView();
+            this.colLastname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colFirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colPhone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAddName = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.formatToolStripMenuItem,
+            this.listViewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // listViewToolStripMenuItem
+            // 
+            this.listViewToolStripMenuItem.Name = "listViewToolStripMenuItem";
+            this.listViewToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.listViewToolStripMenuItem.Text = "ListView";
+            // 
+            // lblDefail
+            // 
+            this.lblDefail.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDefail.Location = new System.Drawing.Point(12, 44);
+            this.lblDefail.Name = "lblDefail";
+            this.lblDefail.Size = new System.Drawing.Size(158, 34);
+            this.lblDefail.TabIndex = 2;
+            this.lblDefail.Text = "Defails";
+            this.lblDefail.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lvThongTin
+            // 
+            this.lvThongTin.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colLastname,
+            this.colFirstName,
+            this.colPhone});
+            this.lvThongTin.GridLines = true;
+            this.lvThongTin.HideSelection = false;
+            this.lvThongTin.Location = new System.Drawing.Point(0, 81);
+            this.lvThongTin.Name = "lvThongTin";
+            this.lvThongTin.Size = new System.Drawing.Size(270, 368);
+            this.lvThongTin.TabIndex = 3;
+            this.lvThongTin.UseCompatibleStateImageBehavior = false;
+            this.lvThongTin.View = System.Windows.Forms.View.Details;
+            // 
+            // colLastname
+            // 
+            this.colLastname.Text = "LastName";
+            // 
+            // colFirstName
+            // 
+            this.colFirstName.Text = "FirstName";
+            // 
+            // colPhone
+            // 
+            this.colPhone.Text = "Phone";
+            // 
+            // listView1
+            // 
+            this.listView1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(433, 107);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(248, 290);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(492, 161);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(55, 13);
+            this.lblLastName.TabIndex = 5;
+            this.lblLastName.Text = "LastName";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(487, 223);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(54, 13);
+            this.lblFirstName.TabIndex = 6;
+            this.lblFirstName.Text = "FirstName";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(487, 296);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 7;
+            this.lblName.Text = "Phone";
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(487, 186);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(143, 20);
+            this.txtLastname.TabIndex = 8;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(487, 253);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(143, 20);
+            this.txtFirstName.TabIndex = 9;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(487, 321);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(143, 20);
+            this.txtPhone.TabIndex = 10;
+            // 
+            // btnAddName
+            // 
+            this.btnAddName.Location = new System.Drawing.Point(578, 360);
+            this.btnAddName.Name = "btnAddName";
+            this.btnAddName.Size = new System.Drawing.Size(84, 26);
+            this.btnAddName.TabIndex = 11;
+            this.btnAddName.Text = "AddName";
+            this.btnAddName.UseVisualStyleBackColor = true;
+            this.btnAddName.Click += new System.EventHandler(this.btnAddName_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Italia.jpg");
+            this.imageList1.Images.SetKeyName(1, "My.jpg");
+            this.imageList1.Images.SetKeyName(2, "Philippine.jpg");
+            this.imageList1.Images.SetKeyName(3, "Vietnam.jpg");
+            // 
+            // frm_Bai5_C2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAddName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lvThongTin);
+            this.Controls.Add(this.lblDefail);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frm_Bai5_C2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form_Bai5_C2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listViewToolStripMenuItem;
+        private System.Windows.Forms.Label lblDefail;
+        private System.Windows.Forms.ListView lvThongTin;
+        private System.Windows.Forms.ColumnHeader colLastname;
+        private System.Windows.Forms.ColumnHeader colFirstName;
+        private System.Windows.Forms.ColumnHeader colPhone;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAddName;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ Winform_Tuan4/Form_Bai5_C2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_Tuan4
+{
+    public partial class frm_Bai5_C2 : Form
+    {
+        public frm_Bai5_C2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddName_Click(object sender, EventArgs e)
+        {
+            ListViewItem items = new ListViewItem();
+            items.Text=txtLastname.Text;
+            items.ImageIndex = 1;
+            items.SubItems.Add(txtFirstName.Text);
+            items.SubItems.Add(txtPhone.Text);
+            lvThongTin.Items.Add(items);
+        }
+    }
+}
